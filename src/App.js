0e0b9f219428a03fe26309f1b87092d059bdda6b
brett f <1brettf@gmail.com>
2019-12-16 15:10:16 -0800

    Getting things going

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Route, NavLink, Link } from 'react-router-dom';
+import Nav from './comp/Nav';
+import Footer from './comp/Footer';
 
 function App() {
   return (
     <div>
-      <h3>Hello</h3>
+      <h3>Main App</h3>
+      <Nav />
+      <Footer />
     </div>
   );
 }
---
+++ src/comp/Nav.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Route, NavLink, Link } from 'react-router-dom';
+
+export default function Nav(){
+  return (
+    <h3>Nav</h3>
+  );
+}
