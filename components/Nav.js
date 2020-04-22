4610e9e04859ae524f344c6ac7ab011f9f2a1826
brett f <1brettf@gmail.com>
2020-04-21 21:00:35 -0700

    Shrink h1 so it's properly bold

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Nav = () => {
   return (
     <nav>
-      <h1 className="logo">BRETT FLORA</h1>
+      <h1>BRETT FLORA</h1>
       <div className="links">
         <a href="https://linkedin.com/in/notbrett">LinkedIn</a>
         <a href="https://github.com/notbrett">Github</a>
