c974454eb9ba96c85df62877c437ce85f761e014
brett f <1brettf@gmail.com>
2020-05-03 13:19:58 -0700

    Update text on buttons on projects

--- components/Project.js
+++ components/Project.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-export default function(props) {
+export default function (props) {
   return (
     <div className="card">
       <img src={props.img} />
       <div className="sources">
         <a target="_blank" href={props.app}>
-          View App
+          Live App
         </a>
         <a target="_blank" href={props.code}>
-          View Code
+          Github
         </a>
       </div>
       <div className="text">
