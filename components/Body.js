3df845c097d8ac971c60f122ac6e59c02366e830
brett f <1brettf@gmail.com>
2020-10-27 21:50:26 -0700

    Update project links to subdomain

--- components/Body.js
+++ components/Body.js
@@ -9,21 +9,21 @@ const Body = () => {
         img="/poem.jpg"
         title="Horse_eBooks Poem Generator"
         desc="This app enables (somewhat) randomized poem generation from the tweets of defunct Twitter spam-bot @horse_ebooks. The front-end is built with React and styled using Emotion.js (CSS-in-JS). The backend is built on Next.js to handle routing, server-side rendering, and perform serverless functions. User-generated poems can be saved and later retrieved (when sharing the poem’s unique link) from a NoSQL database. The app is deployed on Vercel."
-        app="https://epoems.now.sh"
+        app="https://poems.1brett.com"
         code="https://github.com/bx10f/poems"
       />
       <Project
         img="/bible.jpg"
         title="Bible Translation Medley"
         desc="This app retrieves Bible passages and enables dynamic swapping of individual verses. Each swap is a different translation than the original and is placed in the flow of the original passage. The new, swapped passage is now a medley of various translations providing a unique reading experience and potential new insights on the text. The front-end is built with React, Redux. The backend is built on Node.js with Express.js for API routing and querying different Bible APIs."
-        app="https://medley.now.sh"
+        app="https://medley.1brett.com"
         code="https://github.com/bx10f/translation-medley"
       />
       <Project
         img="/tools.jpg"
         title="Foreman Tool Tracker"
         desc="The Tool Tracker gives foremen at Total Mechanical, a Vancouver, WA-based construction company, a mobile-first web experience for viewing, reporting, and requesting tools from the central shop while onsite at a job. The shop manages all the tools, tracking, and reporting through Airtable, which acts as the backend. The front-end is built with React, React Router and styled using Bootstrap / Reactstrap. The front-end is deployed on Vercel and consumes the shop’s Airtable API."
-        app="https://tmtools.now.sh"
+        app="https://tools.1brett.com"
         code="https://github.com/bx10f/tools"
       />
     </div>
