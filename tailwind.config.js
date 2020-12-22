1fe08acb411cb8e682d06c7c8243c00c6acffb51
Brett Flora <1brettf@gmail.com>
2020-12-21 22:41:49 -0800

    Update tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,38 @@
-const defaultTheme = require("tailwindcss/defaultTheme")
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
-  darkMode: "class", // false or 'media' or 'class'
+  purge: ['./pages/**/*.js', './components/**/*.js'],
+  darkMode: 'class', // false or 'media' or 'class'
   theme: {
     screens: {
-      xs: "490px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1496px"
+      xs: '490px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1496px'
     },
     extend: {
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans]
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
       },
       gridTemplateColumns: {
-        footer: "3fr 2fr",
-        subfooter: "2fr 1fr"
+        footer: '3fr 2fr',
+        subfooter: '2fr 1fr'
       },
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            color: theme("colors.gray.900"),
+            color: theme('colors.gray.900'),
             a: {
-              color: theme("colors.blue.600"),
-              "&:hover": {
-                textDecoration: "none"
+              color: theme('colors.blue.600'),
+              '&:hover': {
+                textDecoration: 'none'
               }
             },
-            "h1,h2,h3,h4,h5,h6": {
+            'h1,h2,h3,h4,h5,h6': {
               fontWeight: 900,
-              "scroll-margin-top": defaultTheme.spacing[32]
+              'scroll-margin-top': defaultTheme.spacing[32]
             },
             // p: {
             // fontFamily: theme("fontFamily.serif").join(", ")
@@ -44,22 +44,21 @@ module.exports = {
         },
         dark: {
           css: {
-            color: theme("colors.gray.100"),
+            color: theme('colors.gray.100'),
             a: {
-              color: theme("colors.blue.300")
+              color: theme('colors.blue.300')
             },
-            "h1,h2,h3,h4,h5,h6": {
-              color: theme("colors.gray.100")
-              // "scroll-margin-top": defaultTheme.spacing[32]
+            'h1,h2,h3,h4,h5,h6': {
+              color: theme('colors.gray.100')
             },
-            strong: { color: theme("colors.gray.200") }
+            strong: { color: theme('colors.gray.200') }
           }
         }
       })
     }
   },
   variants: {
-    typography: ["responsive", "dark"]
+    typography: ['responsive', 'dark']
   },
-  plugins: [require("@tailwindcss/typography")]
+  plugins: [require('@tailwindcss/typography')]
 }
