171945e0f622d81904aa7fa4583c985f49ecccee
Brett Flora <1brettf@gmail.com>
2020-12-22 21:31:33 -0800

    Better favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,16 +5,13 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link href="/favicons/favicon.ico" rel="shortcut icon" />
-          <link href="/favicons/site.webmanifest" rel="manifest" />
+          <link href="/favicons/favicon.svg" type="image/svg+xml" rel="shortcut icon" />
+          <link href="/favicons/favicon.ico" rel="alternate  icon" />
           <link href="/favicons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
           <link href="/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
           <link href="/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
-          <link color="#4a9885" href="/favicons/safari-pinned-tab.svg" rel="mask-icon" />
-          <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+          <link color="#ffffff" href="/favicons/safari-pinned-tab.svg" rel="mask-icon" />
           <meta content="#ffffff" name="theme-color" />
-          <meta content="#ffffff" name="msapplication-TileColor" />
-          <meta content="/favicons/browserconfig.xml" name="msapplication-config" />
           <meta
             name="description"
             content="Software engineer, former UX Designer, and avocado enthusiast."
