aab756b349b2c2f8dfdbfa13dada6b363633de0e
Brett Flora <1brettf@gmail.com>
2021-02-12 19:48:36 -0800

    Tweaks

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ module.exports = {
     domains: []
   },
   webpack: (config, { dev, isServer }) => {
+    // if (isServer) {
+    //   require('./scripts/generate-sitemap')
+    // }
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
         react: 'preact/compat',
